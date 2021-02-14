cd1ee60d1ac8edbebe8de116eb6898ea00174475
cflip <36554078+cflip@users.noreply.github.com>
2021-02-14 13:14:54 -0700

    Trowel can only be used when player is present

--- src/main/java/com/jusipat/castleblocks/item/TrowelItem.java
+++ src/main/java/com/jusipat/castleblocks/item/TrowelItem.java
@@ -8,7 +8,6 @@ import net.minecraft.block.Blocks;
 import net.minecraft.client.item.TooltipContext;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.ItemUsage;
 import net.minecraft.item.ItemUsageContext;
 import net.minecraft.text.Text;
 import net.minecraft.text.TranslatableText;
@@ -16,7 +15,6 @@ import net.minecraft.util.ActionResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.registry.Registry;
 import net.minecraft.world.World;
-
 
 import java.util.List;
 
@@ -38,7 +36,7 @@ public class TrowelItem extends Item {
 		World world = context.getWorld();
 		BlockState blockState = world.getBlockState(blockPos);
 
-		if (!world.isClient()) {
+		if (!world.isClient() && context.getPlayer() != null) {
 			boolean isStone = Registry.BLOCK.getId(blockState.getBlock()).equals(Registry.BLOCK.getId(Blocks.STONE));
 			if (isStone) {
 				world.setBlockState(blockPos, ModBlocks.CASTLE_BRICKS.getDefaultState());
