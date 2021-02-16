10752837bc2ab32e32fc36a9d09179fba53e8ef3
cflip <36554078+cflip@users.noreply.github.com>
2021-02-16 10:17:25 -0700

    Add trowel item in

--- src/main/java/com/jusipat/castleblocks/registry/ModItems.java
+++ src/main/java/com/jusipat/castleblocks/registry/ModItems.java
@@ -1,22 +1,13 @@
 package com.jusipat.castleblocks.registry;
 
+import com.jusipat.castleblocks.item.TrowelItem;
+import cpw.mods.fml.common.registry.GameRegistry;
+import net.minecraft.item.Item;
+
 public class ModItems {
-//	public static final BlockItem CASTLE_BRICKS = new BlockItem(ModBlocks.CASTLE_BRICKS, new Item.Settings().group(CastleBlocksMod.CASTLEBLOCKS_GROUP));
-//	public static final BlockItem ANDESITE_CASTLE_BRICKS = new BlockItem(ModBlocks.ANDESITE_CASTLE_BRICKS, new Item.Settings().group(CastleBlocksMod.CASTLEBLOCKS_GROUP));
-//	public static final BlockItem DIORITE_CASTLE_BRICKS = new BlockItem(ModBlocks.DIORITE_CASTLE_BRICKS, new Item.Settings().group(CastleBlocksMod.CASTLEBLOCKS_GROUP));
-//	public static final BlockItem GRANITE_CASTLE_BRICKS = new BlockItem(ModBlocks.GRANITE_CASTLE_BRICKS, new Item.Settings().group(CastleBlocksMod.CASTLEBLOCKS_GROUP));
-//	public static final BlockItem SANDSTONE_CASTLE_BRICKS = new BlockItem(ModBlocks.SANDSTONE_CASTLE_BRICKS, new Item.Settings().group(CastleBlocksMod.CASTLEBLOCKS_GROUP));
-//
-//	public static final TrowelItem TROWEL = new TrowelItem(new FabricItemSettings().group(CastleBlocksMod.CASTLEBLOCKS_GROUP));
+	public static final Item TROWEL = new TrowelItem().setUnlocalizedName("trowel").setTextureName("castleblocks:trowel");
 
 	public static void registerItems() {
-//		GameRegistry.registerItem(TROWEL, "trowel", CastleBlocksMod.MOD_ID);
-//		Registry.register(Registry.ITEM, new Identifier(CastleBlocksMod.MOD_ID, "castle_bricks"), CASTLE_BRICKS);
-//		Registry.register(Registry.ITEM, new Identifier(CastleBlocksMod.MOD_ID, "andesite_castle_bricks"), ANDESITE_CASTLE_BRICKS);
-//		Registry.register(Registry.ITEM, new Identifier(CastleBlocksMod.MOD_ID, "diorite_castle_bricks"), DIORITE_CASTLE_BRICKS);
-//		Registry.register(Registry.ITEM, new Identifier(CastleBlocksMod.MOD_ID, "granite_castle_bricks"), GRANITE_CASTLE_BRICKS);
-//		Registry.register(Registry.ITEM, new Identifier(CastleBlocksMod.MOD_ID, "sandstone_castle_bricks"), SANDSTONE_CASTLE_BRICKS);
-//
-//		Registry.register(Registry.ITEM, new Identifier(CastleBlocksMod.MOD_ID, "trowel"), TROWEL);
+		GameRegistry.registerItem(TROWEL, "trowel");
 	}
 }
