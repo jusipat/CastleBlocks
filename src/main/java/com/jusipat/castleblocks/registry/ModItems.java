eabe1e83c6f5b80c2834c184166f840a66b5a70f
cflip <36554078+cflip@users.noreply.github.com>
2021-02-16 11:43:07 -0700

    Add crafting recipe for trowel

--- src/main/java/com/jusipat/castleblocks/registry/ModItems.java
+++ src/main/java/com/jusipat/castleblocks/registry/ModItems.java
@@ -3,12 +3,20 @@ package com.jusipat.castleblocks.registry;
 import com.jusipat.castleblocks.CastleBlocksMod;
 import com.jusipat.castleblocks.item.TrowelItem;
 import cpw.mods.fml.common.registry.GameRegistry;
+import net.minecraft.init.Items;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 
 public class ModItems {
 	public static final Item TROWEL = new TrowelItem().setUnlocalizedName("trowel").setTextureName("castleblocks:trowel").setMaxStackSize(1).setMaxDamage(TrowelItem.MAX_USES).setCreativeTab(CastleBlocksMod.CASTLEBLOCKS_TAB);
 
 	public static void registerItems() {
 		GameRegistry.registerItem(TROWEL, "trowel");
+		GameRegistry.addShapedRecipe(new ItemStack(TROWEL),
+			" I",
+			"S ",
+			'I', Items.iron_ingot,
+			'S', Items.stick
+		);
 	}
 }
