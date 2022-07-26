bc38363132fab0af1fff59e433b9dc49f4def1e3
justin <justin10patrick@gmail.com>
2022-07-26 14:25:28 -0600

    - Added tree tap local

--- src/main/java/com/jusipat/castleblocks/item/TreeTapItem.java
+++ src/main/java/com/jusipat/castleblocks/item/TreeTapItem.java
@@ -7,19 +7,26 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemUsageContext;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.registry.Registry;
 import net.minecraft.world.World;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 
 
 public class TreeTapItem extends Item {
 
+    boolean extracted = false;
+
+
     public TreeTapItem(Settings settings, int maxUses) {
-        super(settings.maxDamage(maxUses).maxCount(1));
+        super(settings.maxCount(1).maxDamage(maxUses));
 
 
     }
@@ -28,7 +35,7 @@ public class TreeTapItem extends Item {
     public ActionResult useOnBlock(ItemUsageContext context) {
 
         Random rand = new Random();
-        int maxN = 3;
+        int maxN = 2;
         int n = rand.nextInt(maxN) + 1;
 
         BlockPos blockPos = context.getBlockPos();
@@ -40,20 +47,20 @@ public class TreeTapItem extends Item {
 
         if (player != null) {
             Identifier blockId = Registry.BLOCK.getId(blockState.getBlock());
-
-            if (blockId.equals(Registry.BLOCK.getId(Blocks.STRIPPED_BIRCH_LOG)) || blockId.equals(Blocks.STRIPPED_ACACIA_LOG) || blockId.equals(Blocks.STRIPPED_JUNGLE_LOG)
-                    || blockId.equals(Blocks.STRIPPED_DARK_OAK_LOG) || blockId.equals(Blocks.STRIPPED_SPRUCE_LOG) || blockId.equals(Blocks.STRIPPED_MANGROVE_LOG) || blockId.equals(Blocks.STRIPPED_OAK_LOG)) {
-
-                context.getStack().damage(1, player, playerEntity -> playerEntity.sendToolBreakStatus(context.getHand()));
-
-                if (player.getInventory().getEmptySlot() == -1) {
-                    player.dropStack(stack);
-                } else {
-                    player.giveItemStack(stack);
+            if (!world.isClient) {
+                if (blockId.equals(Registry.BLOCK.getId(Blocks.STRIPPED_OAK_LOG)) && !extracted) {
+                    world.playSound(null, blockPos, SoundEvents.BLOCK_SLIME_BLOCK_HIT, SoundCategory.BLOCKS, .5f, 1f);
+                    world.playSound(null, blockPos, SoundEvents.BLOCK_BEEHIVE_SHEAR, SoundCategory.BLOCKS, .5f, 1f);
+                    context.getStack().damage(1, player, playerEntity -> playerEntity.sendToolBreakStatus(context.getHand()));
+                    if (player.getInventory().getEmptySlot() == -1) {
+                        player.dropStack(stack);
+                    } else {
+                        player.giveItemStack(stack);
+                    }
+                    extracted = true; //TODO: disable this!
+                    return ActionResult.SUCCESS;
                 }
-
             }
-            return ActionResult.SUCCESS;
         }
         return ActionResult.PASS;
     }
