aef3d3531cf43f636bced825d7bcd4f700d2a6e5
justin <justin10patrick@gmail.com>
2022-08-02 21:04:45 -0600

    - Removed unused imports - Add generic super for TrowelItem

---
+++ src/main/java/com/jusipat/castleblocks/block/item/TrowelItem.java
@@ -0,0 +1,10 @@
+package com.jusipat.castleblocks.block.item;
+
+import net.minecraft.world.item.Item;
+
+public class TrowelItem extends Item {
+    public TrowelItem(Properties properties, int maxUses) {
+        super(properties);
+    }
+
+}
--- src/main/java/com/jusipat/castleblocks/registry/ModItems.java
+++ src/main/java/com/jusipat/castleblocks/registry/ModItems.java
@@ -1,14 +1,10 @@
 package com.jusipat.castleblocks.registry;
 
 import com.jusipat.castleblocks.CastleBlocksMod;
-import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.level.block.Block;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.RegistryObject;
 
 public final class ModItems {
 
