4663b0456ca9cc2981f38c6390f18674aa0da9b6
justin <justin10patrick@gmail.com>
2023-01-09 18:04:10 -0700

    Castle bricks block breaking functionality semi-implementation

--- src/main/java/com/jusipat/castleblocks/block/CastleBlock.java
+++ src/main/java/com/jusipat/castleblocks/block/CastleBlock.java
@@ -1,8 +1,6 @@
 package com.jusipat.castleblocks.block;
 
 import net.minecraft.core.BlockPos;
-import net.minecraft.world.InteractionHand;
-import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
@@ -38,31 +36,12 @@ public class CastleBlock extends BaseEntityBlock implements BlockEntityType.Bloc
     }
 
     @Override
-    public boolean onDestroyedByPlayer(BlockState state, Level level, BlockPos pos, Player player, boolean willHarvest, FluidState fluid) {
-        BlockEntity blockEntity = level.getBlockEntity(pos);
-
-        if (blockEntity instanceof CastleBlockEntity castleBlockEntity)
-            if(castleBlockEntity.isOwner(player.getUUID()))
-                if(!castleBlockEntity.isOwner(player.getUUID())) {
-                    return false;
-                }
-
-        return super.onDestroyedByPlayer(state, level, pos, player, willHarvest, fluid);
-    }
-
-    @Override
-    public InteractionResult use(BlockState blockState, Level level, BlockPos blockPos, Player player, InteractionHand hand, BlockHitResult blockHitResult) {
-        return super.use(blockState, level, blockPos, player, hand, blockHitResult);
-    }
-
-    @Nullable
-    @Override
     public BlockEntity newBlockEntity(BlockPos blockPos, BlockState blockState) {
         return new CastleBlockEntity(blockPos, blockState);
     }
 
     @Override
-    public BlockEntity create(BlockPos blockPos, BlockState blockState) {
+    public BlockEntity create(BlockPos pos, BlockState state) {
         return null;
     }
 }
---
+++ src/main/java/com/jusipat/castleblocks/event/ModEvents.java
@@ -0,0 +1,29 @@
+package com.jusipat.castleblocks.event;
+
+import com.jusipat.castleblocks.CastleBlocksMod;
+import com.jusipat.castleblocks.block.CastleBlockEntity;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraftforge.event.entity.player.PlayerEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+public class ModEvents {
+    @Mod.EventBusSubscriber(modid = CastleBlocksMod.MOD_ID)
+    public static class ForgeEvents {
+
+        @SubscribeEvent
+        public static void onBlockBreak(PlayerEvent.BreakSpeed event) {
+            Level level = event.getPlayer().getLevel();
+            BlockEntity blockEntity = level.getBlockEntity(event.getPos());
+
+            if (blockEntity instanceof CastleBlockEntity castleBlockEntity) {
+                if (castleBlockEntity.isOwner(event.getPlayer().getUUID())) {
+                    event.setNewSpeed(2);
+                } else {
+                    event.setNewSpeed(0.01f);
+                }
+            }
+        }
+    }
+}
