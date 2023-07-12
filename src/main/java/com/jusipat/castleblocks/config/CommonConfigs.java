bc242c198c35055cb3252f728f52860e0ad4869c
talhanation <talhakantar@yahoo.com>
2023-07-12 08:38:30 +0200

    1.0.9.0
    - fixed pvp function would not work correctly
    - added config for hardness modifier
    - 1.19.2 related fixes
    - changed version from 1.0.8.1 to 1.0.9.0

--- src/main/java/com/jusipat/castleblocks/config/CommonConfigs.java
+++ src/main/java/com/jusipat/castleblocks/config/CommonConfigs.java
@@ -4,15 +4,25 @@ import net.minecraftforge.common.ForgeConfigSpec;
 
 public class CommonConfigs {
     public static final ForgeConfigSpec.Builder BUILDER = new ForgeConfigSpec.Builder();
-    public static final ForgeConfigSpec SPEC;
+    public static ForgeConfigSpec SPEC;
 
-    public static final ForgeConfigSpec.BooleanValue PVP_MODE;
+    public static ForgeConfigSpec.DoubleValue MODIFIER;
+    public static ForgeConfigSpec.BooleanValue PVP_MODE;
 
     static {
         BUILDER.push("CastleBlocks Configuration");
 
-        PVP_MODE = BUILDER.comment("Enables if castle bricks are harder to break if not the original placer")
-                .define("true/false", true);
+        PVP_MODE = BUILDER.comment("Enables if castle bricks are harder to break if not the block owner")
+                .define("PVP_MODE", true);
+
+        MODIFIER = BUILDER.comment("""
+                        Hardness modifier if not the block owner:
+                        Higher values, harder to break
+                        Lower values, easier to break
+                        0.0 will break it instantly;
+                        1.0 is Vanilla hardness""")
+                .worldRestart()
+                .defineInRange("Modifier", 10.0, 1.0, 1000.0);
 
         BUILDER.pop();
         SPEC = BUILDER.build();
