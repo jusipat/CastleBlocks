231c56400b1b6dd140ea357138510b45ef635bb6
jusipat <justin10patrick@gmail.com>
2023-09-26 14:24:05 -0600

    Add onBannerInteract event

--- src/main/java/com/jusipat/castleblocks/event/ModEvents.java
+++ src/main/java/com/jusipat/castleblocks/event/ModEvents.java
@@ -3,9 +3,13 @@ package com.jusipat.castleblocks.event;
 import com.jusipat.castleblocks.CastleBlocksMod;
 import com.jusipat.castleblocks.block.CastleBlockEntity;
 import com.jusipat.castleblocks.config.CommonConfigs;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.NameTagItem;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.entity.BannerBlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraftforge.event.entity.player.PlayerEvent;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
@@ -20,9 +24,21 @@ public class ModEvents {
             if (blockEntity instanceof CastleBlockEntity castleBlockEntity && CommonConfigs.PVP_MODE.get()) {
                 if (castleBlockEntity.isOwner(event.getEntity().getUUID())) {
                     event.setNewSpeed(event.getNewSpeed());
-                }
-                else {
+                } else {
                     event.setNewSpeed(event.getNewSpeed() / CommonConfigs.MODIFIER.get().floatValue());
+                }
+            }
+        }
+
+        @SubscribeEvent
+        public static void onBannerInteract(PlayerInteractEvent.RightClickBlock event) {
+            Level level = event.getEntity().level();
+            BlockEntity blockEntity = level.getBlockEntity(event.getPos());
+
+            if (blockEntity instanceof BannerBlockEntity bannerBlockEntity) {
+                ItemStack itemStack = event.getItemStack();
+                if (itemStack.getItem() instanceof NameTagItem nameTagItem) {
+                    System.out.println(itemStack.getHoverName().getString());
                 }
             }
         }
